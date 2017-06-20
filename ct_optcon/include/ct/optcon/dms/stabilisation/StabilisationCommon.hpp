aa57e700d23fd75d37edd7d8292d92885ebb9963
Markus Gft <markusgft@gmail.com>
2017-06-20 18:30:09 +0200

    fixing an inline function

--- ct_optcon/include/ct/optcon/dms/stabilisation/StabilisationCommon.hpp
+++ ct_optcon/include/ct/optcon/dms/stabilisation/StabilisationCommon.hpp
@@ -1,7 +1,7 @@
 #ifndef DMS_STABILISER_TASK_COMMON_HPP_
 #define DMS_STABILISER_TASK_COMMON_HPP_
 
-#include <ct/optcon/dms/dms_core/DmsSettings.hpp>
+//#include <ct/optcon/dms/dms_core/DmsSettings.hpp>
 
 namespace ct {
 namespace optcon {
