ad325a6dd0fdbeb27d3244c7dc7eda4778fbfee3
Markus Gft <markusgft@gmail.com>
2017-06-26 20:45:31 +0200

    MPC test now also for float types

--- ct_optcon/test/mpc/iLQGMPCTest.cpp
+++ ct_optcon/test/mpc/iLQGMPCTest.cpp
@@ -49,73 +49,86 @@ const size_t control_dim = 1; // force
 
 const double kStiffness = 10;
 
-class Dynamics : public ControlledSystem<state_dim, control_dim>
+namespace tpl {
+
+template <typename SCALAR = double>
+class Dynamics : public ControlledSystem<state_dim, control_dim, SCALAR>
 {
 public:
-	Dynamics() : ControlledSystem<state_dim, control_dim>(SYSTEM_TYPE::SECOND_ORDER) {}
+	Dynamics() : ControlledSystem<state_dim, control_dim, SCALAR>(SYSTEM_TYPE::SECOND_ORDER) {}
 
 	void computeControlledDynamics(
-			const StateVector<state_dim>& state,
-			const Time& t,
-			const ControlVector<control_dim>& control,
-			StateVector<state_dim>& derivative
+			const StateVector<state_dim, SCALAR>& state,
+			const SCALAR& t,
+			const ControlVector<control_dim, SCALAR>& control,
+			StateVector<state_dim, SCALAR>& derivative
 	) override
-			{
+	{
 		derivative(0) = state(1);
 		derivative(1) = control(0) - kStiffness*state(0); // mass is 1 kg
-			}
+	}
 
-	Dynamics* clone() const override
-			{
-		return new Dynamics();
-			};
+	Dynamics<SCALAR>* clone() const override
+	{
+		return new Dynamics<SCALAR>();
+	};
 };
 
-class LinearizedSystem : public LinearSystem<state_dim, control_dim>
+
+template <typename SCALAR = double>
+class LinearizedSystem : public LinearSystem<state_dim, control_dim, SCALAR>
 {
 public:
+
+	typedef core::StateMatrix<state_dim, SCALAR> state_matrix_t;
+	typedef core::StateControlMatrix<state_dim, control_dim, SCALAR> state_control_matrix_t;
+
 	state_matrix_t A_;
 	state_control_matrix_t B_;
 
-
-	const state_matrix_t& getDerivativeState(const StateVector<state_dim>& x, const ControlVector<control_dim>& u, const double t = 0.0) override
-			{
+	const state_matrix_t& getDerivativeState(const StateVector<state_dim, SCALAR>& x, const ControlVector<control_dim, SCALAR>& u, const SCALAR t = 0.0) override
+	{
 		A_ << 0, 1, -kStiffness, 0;
 		return A_;
-			}
+	}
 
-	const state_control_matrix_t& getDerivativeControl(const StateVector<state_dim>& x, const ControlVector<control_dim>& u, const double t = 0.0) override
-			{
-
+	const state_control_matrix_t& getDerivativeControl(const StateVector<state_dim, SCALAR>& x, const ControlVector<control_dim, SCALAR>& u, const SCALAR t = 0.0) override
+	{
 		B_ << 0, 1;
 		return B_;
-			}
+	}
 
-	LinearizedSystem* clone() const override
-			{
-		return new LinearizedSystem();
-			};
+	LinearizedSystem<SCALAR>* clone() const override
+	{
+		return new LinearizedSystem<SCALAR>();
+	};
 };
 
-std::shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > createCostFunction(Eigen::Vector2d& x_final) {
-	Eigen::Matrix2d Q;
+
+template <typename SCALAR = double>
+std::shared_ptr<CostFunctionQuadratic<state_dim, control_dim, SCALAR> > createCostFunction(Eigen::Matrix<SCALAR, 2, 1>& x_final)
+{
+	Eigen::Matrix<SCALAR, 2, 2> Q;
 	Q << 0, 0, 0, 0.1;
 
-	Eigen::Matrix<double, 1, 1> R;
+	Eigen::Matrix<SCALAR, 1, 1> R;
 	R << 0.001;
 
-	Eigen::Vector2d x_nominal = Eigen::Vector2d::Zero();
-	Eigen::Matrix<double, 1, 1> u_nominal = Eigen::Matrix<double, 1, 1>::Zero();
+	Eigen::Matrix<SCALAR, 2, 1> x_nominal; x_nominal.setZero();
+	Eigen::Matrix<SCALAR, 1, 1> u_nominal; u_nominal.setZero();
 
-	Eigen::Matrix2d Q_final;
+	Eigen::Matrix<SCALAR, 2, 2> Q_final;
 	Q_final << 1000, 0, 0, 1000;
 
-	std::shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > quadraticCostFunction(
-			new CostFunctionQuadraticSimple<state_dim, control_dim>(
+	std::shared_ptr<CostFunctionQuadratic<state_dim, control_dim, SCALAR> > quadraticCostFunction(
+			new CostFunctionQuadraticSimple<state_dim, control_dim, SCALAR>(
 					Q, R, x_nominal, u_nominal, x_final, Q_final));
 
 	return quadraticCostFunction;
 }
+
+} // namespace tpl
+
 
 
 /**
@@ -123,19 +136,22 @@ std::shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > createCostFuncti
  */
 TEST(MPCTest, PreIntegratorTest)
 {
+	typedef tpl::Dynamics<double> Dynamics;
+	typedef tpl::LinearizedSystem<double> LinearizedSystem;
+
 	try {
 
 		// desired final state
-		Eigen::Vector2d x_final; x_final << 20, 0;
+		Eigen::Matrix<double, state_dim, 1> x_final; x_final << 20, 0;
 
 		StateVector<state_dim> x0; x0.setRandom();	// init state
-		ct::core::Time timeHorizon = 3.0;	// final time
+		double timeHorizon = 3.0;	// final time
 
 
 		// set up the Optimal Control Problem
 		shared_ptr<ControlledSystem<state_dim, control_dim> > nonlinearSystem(new Dynamics);
 		shared_ptr<LinearSystem<state_dim, control_dim> > analyticLinearSystem(new LinearizedSystem);
-		shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > costFunction = createCostFunction(x_final);
+		shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > costFunction = tpl::createCostFunction<double>(x_final);
 
 		OptConProblem<state_dim, control_dim> optConProblem (nonlinearSystem, costFunction, analyticLinearSystem);
 		optConProblem.setInitialState(x0);
@@ -246,8 +262,11 @@ TEST(MPCTest, PreIntegratorTest)
 
 
 
-TEST(MPCTest, iLQGMPC)
+TEST(MPCTest, iLQGMPC_DoublePrecision)
 {
+	typedef tpl::Dynamics<double> Dynamics;
+	typedef tpl::LinearizedSystem<double> LinearizedSystem;
+
 	try {
 
 		Eigen::Vector2d x_final; x_final << 20, 0;
@@ -259,7 +278,7 @@ TEST(MPCTest, iLQGMPC)
 		// set up the Optimal Control Problem
 		shared_ptr<ControlledSystem<state_dim, control_dim> > nonlinearSystem(new Dynamics);
 		shared_ptr<LinearSystem<state_dim, control_dim> > analyticLinearSystem(new LinearizedSystem);
-		shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > costFunction = createCostFunction(x_final);
+		shared_ptr<CostFunctionQuadratic<state_dim, control_dim> > costFunction = tpl::createCostFunction<double>(x_final);
 
 		OptConProblem<state_dim, control_dim> optConProblem (nonlinearSystem, costFunction, analyticLinearSystem);
 
@@ -441,6 +460,184 @@ TEST(MPCTest, iLQGMPC)
 #endif
 
 
+	} catch (std::exception& e)
+	{
+		std::cout << "caught exception: "<<e.what() <<std::endl;
+		FAIL();
+	}
+}
+
+
+
+
+
+
+TEST(MPCTest, iLQGMPC_SinglePrecision)
+{
+	typedef tpl::Dynamics<float> Dynamics;
+	typedef tpl::LinearizedSystem<float> LinearizedSystem;
+
+	try {
+
+		Eigen::Vector2f x_final; x_final << 20, 0;
+
+		StateVector<state_dim, float> x0; x0.setRandom();
+
+		float timeHorizon = 3.0;
+
+		// set up the Optimal Control Problem
+		shared_ptr<ControlledSystem<state_dim, control_dim, float> > nonlinearSystem(new Dynamics);
+		shared_ptr<LinearSystem<state_dim, control_dim, float> > analyticLinearSystem(new LinearizedSystem);
+		shared_ptr<CostFunctionQuadratic<state_dim, control_dim, float> > costFunction = tpl::createCostFunction<float>(x_final);
+
+		OptConProblem<state_dim, control_dim, float> optConProblem (nonlinearSystem, costFunction, analyticLinearSystem);
+
+		optConProblem.setTimeHorizon(timeHorizon);
+
+		optConProblem.setInitialState(x0);
+
+
+		// FIRST ILQG INSTANCE FOR CALCULATING THE 'PERFECT' INITIAL GUESS
+
+		iLQGSettings ilqg_settings;
+		ilqg_settings.dt = 0.001;
+		ilqg_settings.dt_sim = 0.001;
+		ilqg_settings.max_iterations = 10000000;
+		ilqg_settings.min_cost_improvement = 0.0;	// strict bounds to reach a solution very close to optimality
+
+		size_t K = std::round(timeHorizon / ilqg_settings.dt); // number of steps
+
+
+		// provide initial controller
+		FeedbackArray<state_dim, control_dim, float> u0_fb(K, FeedbackMatrix<state_dim, control_dim, float>::Zero());
+
+		ControlVectorArray<control_dim, float> u0_ff(K, ControlVector<control_dim, float>::Zero());
+
+		ct::core::StateFeedbackController<state_dim, control_dim, float> initController (u0_ff, u0_fb, ilqg_settings.dt);
+
+
+		// solve iLQG and obtain perfect init guess
+		iLQG<state_dim, control_dim, float> iLQG_init (optConProblem, ilqg_settings);
+
+		iLQG_init.configure(ilqg_settings);
+
+		iLQG_init.setInitialGuess(initController);
+
+		iLQG_init.solve();
+
+		ct::core::StateFeedbackController<state_dim, control_dim, float> perfectInitController = iLQG_init.getSolution();
+		ct::core::StateTrajectory<state_dim, float> perfectStateTrajectory = iLQG_init.getStateTrajectory();
+
+
+		// settings for the ilqg instance used in MPC
+		iLQGSettings ilqg_settings_mpc;
+		ilqg_settings_mpc.dt = 0.001;
+		ilqg_settings_mpc.dt_sim = 0.001;
+		ilqg_settings_mpc.max_iterations = 5;
+		ilqg_settings_mpc.discretization = iLQGSettings::FORWARD_EULER;
+
+
+		// mpc specific settings
+		ct::optcon::mpc_settings settings;
+		settings.stateForwardIntegration_ = true;
+		settings.postTruncation_ = true;
+		settings.measureDelay_ = true;
+		settings.delayMeasurementMultiplier_ = 1.0;
+		settings.mpc_mode = ct::optcon::MPC_MODE::FIXED_FINAL_TIME;
+		settings.coldStart_ = false;
+		settings.additionalDelayUs_ = 0;
+
+
+		// Create MPC object
+		MPC<iLQG<state_dim, control_dim, float>> ilqg_mpc (optConProblem, ilqg_settings_mpc, settings);
+
+		ilqg_mpc.setInitialGuess(perfectInitController);
+		ilqg_mpc.setStateTrajectory(perfectStateTrajectory);
+
+
+		// fake the time -- here the start time
+		auto start_time = std::chrono::high_resolution_clock::now();
+
+		// outputs
+		std::vector<ct::core::StateTrajectory<state_dim, float>> stateTrajContainer;	// collection of all state trajectories
+		ct::core::StateTrajectory<state_dim, float> tempStateTraj;
+		std::vector<float> timeStamps;	// collection of all policy-start timestamps
+		std::vector<ct::core::StateVector<state_dim, float>> initStates; // collection of all initial tests
+
+
+		size_t maxNumRuns = 2000;
+		size_t numRuns = 0;
+
+		std::cout << "Starting to run MPC" << std::endl;
+
+		for(size_t i = 0; i<maxNumRuns; i++)
+		{
+
+			// we assume to have a perfect initial state (perfect state evolution)
+			if(i>0)
+				x0 = tempStateTraj.front();
+
+			// time which has passed since start of MPC
+			auto current_time = std::chrono::high_resolution_clock::now();
+			float t = 0.000001*std::chrono::duration_cast<std::chrono::microseconds>(current_time - start_time).count();
+
+
+			// new optimal policy
+			ct::core::StateFeedbackController<state_dim, control_dim, float> newPolicy;
+
+			// timestamp of the new optimal policy
+			float ts_newPolicy;
+
+
+			// run one mpc cycle
+			bool success = ilqg_mpc.run(x0, t, newPolicy, ts_newPolicy);
+
+
+			tempStateTraj = ilqg_mpc.getStateTrajectory();
+
+			// we save every 20-th trajectory
+			if(i%20 == 0)
+			{
+				stateTrajContainer.push_back(tempStateTraj);
+				timeStamps.push_back(ts_newPolicy);
+				initStates.push_back(x0);
+			}
+
+
+			if(ilqg_mpc.timeHorizonReached() || !success)
+				break;
+
+			numRuns++;
+		}
+
+
+		ilqg_mpc.printMpcSummary();
+
+		ASSERT_GT(numRuns, 10); // make sure that MPC runs more than 10 times
+
+
+		/*
+		 * Intuition:
+		 * The start of every mpc state trajectory must lie on the initial "perfect" state trajectory,
+		 * since we have negligible delays here, a close-to-perfect state 'measurement' and no perturbations.
+		 * the perfect state trajectory above starts at t=0 and the init time-stamp
+		 * of the first MPC solution is the following
+		 * */
+		float mpcTimeOffset = timeStamps.front();
+		std::cout << "mpc trajectories time offset due to init solve: " << mpcTimeOffset << std::endl;
+
+		for(size_t i = 0; i< stateTrajContainer.size(); i++)
+		{
+			float relTime = timeStamps[i] - mpcTimeOffset;
+			ct::core::StateVector<state_dim, float> mpcTrajInitState = stateTrajContainer[i].front();
+			ct::core::StateVector<state_dim, float> refState = stateTrajContainer[0].eval(relTime);
+
+//			std::cout << "x_ref: " << refState.transpose() << std::endl;
+//			std::cout << "x_mpc: " << mpcTrajInitState.transpose() << std::endl;
+
+			ASSERT_LT(std::fabs((refState-mpcTrajInitState)(0)), 1.0);	// max pos deviation
+			ASSERT_LT(std::fabs((refState-mpcTrajInitState)(1)), 1.0);	// max vel deviation
+		}
 	} catch (std::exception& e)
 	{
 		std::cout << "caught exception: "<<e.what() <<std::endl;
