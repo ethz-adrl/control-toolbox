40cc3f997e5682c4bbf1684cb7684ef2960373d6
Markus Gft <markusgft@gmail.com>
2017-07-06 16:31:23 +0200

    nicer plot for gnms example

--- ct_optcon/include/ct/optcon/gnms/matlab/gnmsPlot.m
+++ ct_optcon/include/ct/optcon/gnms/matlab/gnmsPlot.m
@@ -1,22 +1,45 @@
 clear all
 close all
-load ../../../../../../../ct/GNMSLog2.mat
+load ../../../../../../../ct/GNMSLog0.mat
 
 %reformat
 t = squeeze(t);
 lv = squeeze(lv);
 
-
-plot(t, x(1,:))
+figure()
+subplot(2,2,1)
+plot(t, x(1,:), 'kx', 'MarkerSize',1)
 hold on;
-plot(t, xShot(1,:))
-plot(t, x_rollout(1,:))
-legend('x', 'xShot', 'xrollout');
+plot(t, xShot(1,:),  'bx', 'MarkerSize',1)
+legend('x*', 'x init');
+xlabel('t [sec]')
+ylabel('x [m]')
+title('positions');
 
-%%
-figure();
+subplot(2,2,2)
+plot(t, x(2,:), 'k')
+hold on;
+plot(t, xShot(2,:),  'b')
+legend('v*', 'v init');
+xlabel('t [sec]')
+ylabel('v [m/sec]')
+title('velocities');
+
+
+subplot(2,2,3)
 plot(t(1:end-1), lv)
-title('gnms lv')
+title('control update')
+xlabel('t [sec]')
+ylabel('F [N]')
+
+subplot(2,2,4)
+plot(t, lx(1,:)); hold on
+plot(t, lx(2,:)); hold on
+title('state update')
+xlabel('t [sec]')
+ylabel('dx [m]')
+legend('x update', 'v update')
+
 
 %%
 figure();
@@ -24,7 +47,3 @@ plot(t(1:end-1), d(1,:)); hold on;
 %plot(t(1:end-1), d(2,:))
 title('defect')
 
-%%
-figure()
-plot(t, lx(1,:))
-title('gnms lx')
