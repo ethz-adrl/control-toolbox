449d3e489b331928edf3f64acde697a6c8ff96c3
Markus Gft <markusgft@gmail.com>
2017-07-13 19:52:18 +0200

    adding some licence files

--- ct_optcon/include/ct/optcon/solver/implementation/NLOptConSolver-impl.hpp
+++ ct_optcon/include/ct/optcon/solver/implementation/NLOptConSolver-impl.hpp
@@ -1,13 +1,28 @@
-/*
- * NLOptConSolver-impl.hpp
- *
- * Created on: 13.07.2017
- * 	   Author: mgiftthaler<mgiftthaler@ethz.ch> 
- * 
- */
+/***********************************************************************************
+Copyright (c) 2017, Michael Neunert, Markus Giftthaler, Markus Stäuble, Diego Pardo,
+Farbod Farshidian. All rights reserved.
 
-#ifndef NLOPTCONSOLVER_IMPL_HPP_
-#define NLOPTCONSOLVER_IMPL_HPP_
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of ETH ZURICH nor the names of its contributors may be used
+      to endorse or promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL ETH ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************************/
 
 
 namespace ct{
@@ -22,12 +37,12 @@ void NLOptConSolver<STATE_DIM, CONTROL_DIM, SCALAR>::initialize(const OptConProb
 		//	nlocBackend_ = std::shared_ptr<NLOCBackendBase<STATE_DIM, CONTROL_DIM>>(new NLOCBackendMP<STATE_DIM, CONTROL_DIM>(optConProblem, settings));
 		throw std::runtime_error("Selection of MP algorithms not implemented");
 	else
-		nlocBackend_ = std::shared_ptr<NLOCBackendBase<STATE_DIM, CONTROL_DIM>>(new NLOCBackendST<STATE_DIM, CONTROL_DIM>(optConProblem, settings));
-
+		nlocBackend_ = std::shared_ptr<NLOCBackendBase<STATE_DIM, CONTROL_DIM>>(
+				new NLOCBackendST<STATE_DIM, CONTROL_DIM>(optConProblem, settings));
 
 	configure(settings);
 
-} // configure()
+}
 
 template <size_t STATE_DIM, size_t CONTROL_DIM, typename SCALAR>
 void NLOptConSolver<STATE_DIM, CONTROL_DIM, SCALAR>::configure(const Settings_t& settings)
@@ -38,7 +53,8 @@ void NLOptConSolver<STATE_DIM, CONTROL_DIM, SCALAR>::configure(const Settings_t&
 	switch(settings.nlocp_algorithm)
 	{
 	case NLOptConSettings::NLOCP_ALGORITHM::GNMS:
-		nlocAlgorithm_ = std::shared_ptr<NLOCAlgorithm<STATE_DIM, CONTROL_DIM>> ( new GNMS_CT<STATE_DIM, CONTROL_DIM, SCALAR>(nlocBackend_, settings) );
+		nlocAlgorithm_ = std::shared_ptr<NLOCAlgorithm<STATE_DIM, CONTROL_DIM>> (
+				new GNMS_CT<STATE_DIM, CONTROL_DIM, SCALAR>(nlocBackend_, settings) );
 		break;
 	default:
 		throw std::runtime_error("This algorithm is not implemented in NLOptConSolver.hpp");
