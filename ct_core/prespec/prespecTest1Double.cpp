16d96a79ed825f0459787e8600e45979f9f7c1ec
Michael Neunert <neunertm@gmail.com>
2017-10-04 00:02:26 +0200

    added IntegrationTestPrespec and two explicit template instantiations

---
+++ ct_core/prespec/prespecTest1Double.cpp
@@ -0,0 +1,13 @@
+/*
+ * prespecTest1Double.cpp
+ *
+ *  Created on: Oct 3, 2017
+ *      Author: neunertm
+ */
+
+#include <ct/core/core.h>
+
+#define STATE_DIM_PRESPEC 1
+#define SCALAR_PRESPEC double
+
+#include <ct/core/core-prespec-helper.h>
---
+++ ct_core/prespec/prespecTest2Double.cpp
@@ -0,0 +1,13 @@
+/*
+ * prespecTest2Double.cpp
+ *
+ *  Created on: Oct 3, 2017
+ *      Author: neunertm
+ */
+
+#include <ct/core/core.h>
+
+#define STATE_DIM_PRESPEC 2
+#define SCALAR_PRESPEC double
+
+#include <ct/core/core-prespec-helper.h>
