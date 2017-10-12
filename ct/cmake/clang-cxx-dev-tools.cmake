8bf85f3e6900051999be382cfed08a11dd933db7
Markus Gft <markusgft@gmail.com>
2017-10-13 00:19:36 +0200

    removing clang-format, as we decided to adopt the Google-style

--- ct/cmake/clang-cxx-dev-tools.cmake
+++ ct/cmake/clang-cxx-dev-tools.cmake
@@ -10,20 +10,20 @@ file(GLOB_RECURSE
           
 
 # Adding clang-format target if executable is found
-find_program(CLANG_FORMAT_BIN "clang-format-3.9")
-message(${CLANG_FORMAT_BIN})
-if(NOT CLANG_FORMAT_BIN)
-    message(WARNING "CLANG-FORMAT not found")
-else(NOT CLANG_FORMAT_BIN)
-  message (WARNING "USING CLANG-FORMAT. This re-formats the source-code in a well-defined style.")
-  add_custom_target(
-    clang-format ALL
-    COMMAND clang-format-3.9
-    -i
-    -style=Google
-    ${ALL_CXX_SOURCE_FILES}
-    )
-endif()
+#find_program(CLANG_FORMAT_BIN "clang-format-3.9")
+#message(${CLANG_FORMAT_BIN})
+#if(NOT CLANG_FORMAT_BIN)
+#    message(WARNING "CLANG-FORMAT not found")
+#else(NOT CLANG_FORMAT_BIN)
+#  message (WARNING "USING CLANG-FORMAT. This re-formats the source-code in a well-defined style.")
+#  add_custom_target(
+#    clang-format ALL
+#    COMMAND clang-format-3.9
+#    -i
+#    -style=Google
+#    ${ALL_CXX_SOURCE_FILES}
+#    )
+#endif()
 
 # Adding clang-tidy target if clang-tidy executable is found
 find_program(CLANG_TIDY_BIN "clang-tidy-3.9")
