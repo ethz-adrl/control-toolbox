722ec382469b16d1e29ac983e8b3704f91df60d9
Markus Gft <markusgft@gmail.com>
2017-10-13 14:38:16 +0200

    refactoring licence headers in ct_core

--- ct_core/src/core/control/PIDController.cpp
+++ ct_core/src/core/control/PIDController.cpp
@@ -25,7 +25,6 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************************/
 
-
 #include <iostream>
 
 #include <ct/core/control/siso/PIDController.h>
