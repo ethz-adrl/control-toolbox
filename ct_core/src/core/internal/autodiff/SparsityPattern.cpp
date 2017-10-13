d28f9bb5ceb7efb3e48e77b6c20b7c3f6f64a82b
Markus Gft <markusgft@gmail.com>
2017-10-13 15:07:02 +0200

    adding missing include

--- ct_core/src/core/internal/autodiff/SparsityPattern.cpp
+++ ct_core/src/core/internal/autodiff/SparsityPattern.cpp
@@ -24,6 +24,8 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************************/
+
+#include <ct/core/core.h>
 #include <ct/core/internal/autodiff/SparsityPattern.h>
 
 namespace ct {
