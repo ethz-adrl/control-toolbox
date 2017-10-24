64161326959ffe18f21db537282a00b873aa6e65
Michael Neunert <neunertm@gmail.com>
2017-10-25 01:57:23 +0200

    changed licence section in files

---
+++ ct_rbd/include/ct/rbd/slq/FloatingBaseSLQ.h
@@ -0,0 +1,123 @@
+/*
+ * FloatingBaseSLQ.h
+ *
+ *  Created on: Jan 11, 2017
+ *      Author: neunertm
+ */
+
+#ifndef INCLUDE_CT_RBD_SLQ_FLOATINGBASESLQ_H_
+#define INCLUDE_CT_RBD_SLQ_FLOATINGBASESLQ_H_
+
+#include <ct/core/systems/linear/SystemLinearizer.h>
+
+#include <ct/optcon/ilqg/iLQGMP.hpp>
+
+#include <ct/rbd/systems/FloatingBaseFDSystem.h>
+
+
+namespace ct{
+namespace rbd {
+
+/**
+ * \brief SLQ for floating base systems without an explicit contact model.
+ * The contact constraint is enforced via a cost function
+ */
+template <class RBDDynamics>
+class FloatingBaseSLQ
+{
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+	static const bool quatIntegration = false;
+	static const bool eeForcesAreControlInputs = true;
+
+	typedef FloatingBaseFDSystem<RBDDynamics, false, true> FBSystem;
+	typedef ct::core::LinearSystem<FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> LinearizedSystem;
+	typedef ct::core::SystemLinearizer<FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> SystemLinearizer;
+
+	typedef ct::optcon::iLQGMP<FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> iLQG;
+	typedef ct::optcon::iLQGMP<FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> iLQGMP;
+
+	typedef ct::optcon::CostFunctionAnalytical<FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> CostFunction;
+	typedef ct::rbd::TermTaskspace<FBSystem::Kinematics, true, FBSystem::STATE_DIM, FBSystem::CONTROL_DIM> TaskSpaceTerm;
+
+	typedef iLQG::StateTrajectory StateTrajectory;
+
+
+	FloatingBaseSLQ(
+			std::shared_ptr<FBSystem> system,
+			const std::string& costFunctionFile,
+			std::shared_ptr<LinearizedSystem> linearizedSystem = nullptr,
+			bool useMP = true
+	) :
+		system_(system),
+		linearizedSystem_(linearizedSystem),
+		useMP_(useMP),
+		iteration_(0)
+	{
+		// if no linearized system provided, use general system linearizer
+		if (linearizedSystem_ == nullptr)
+			linearizedSystem_ = std::shared_ptr<LinearizedSystem> (new SystemLinearizer(system));
+
+		setupCostFunction(costFunctionFile);
+
+		ilqg_ = std::shared_ptr<iLQG>(new iLQG(system_, linearizedSystem_, costFunction_));
+		ilqgMp_ = std::shared_ptr<iLQGMP>(new iLQGMP(system_, linearizedSystem_, costFunction_));
+	}
+
+	bool runIteration(StateTrajectory& stateTrajectory)
+	{
+		bool foundBetter;
+
+		if (useMP_)
+		{
+			foundBetter = ilqgMp_->runIteration(ilqg_settings_, line_search_settings_);
+			stateTrajectory = ilqgMp_->retrieveLastRollout();
+		}
+		else {
+			foundBetter = ilqg_->runIteration(ilqg_settings_, line_search_settings_);
+			stateTrajectory = ilqg_->retrieveLastRollout();
+		}
+
+		iteration_++;
+		return foundBetter;
+	}
+
+	ilqg_settings_t iLQGSettings() { return ilqg_settings_; }
+
+private:
+
+	void setupCostFunction(const std::string& filename)
+	{
+		costFunction_ = std::shared_ptr<CostFunction>(new CostFunction(filename, true));
+
+		for (size_t i=0; i<N_EE; i++)
+		{
+			eePosTerms_.push_back(new TaskSpaceTerm(filename, "eePos"+std::to_string(i), true));
+			costFunction_->addIntermediateTerm(eePosTerms_.back());
+		}
+	}
+
+	std::shared_ptr<FBSystem> system_;
+	std::shared_ptr<LinearizedSystem> linearizedSystem_;
+
+	std::shared_ptr<iLQG> ilqg_;
+	std::shared_ptr<iLQG> ilqgMp_;
+
+	std::shared_ptr<CostFunction> costFunction_;
+	std::vector<std::shared_ptr<TaskSpaceTerm>> eePosTerms_;
+
+	ilqg_settings_t ilqg_settings_;
+	line_search_settings_t line_search_settings_;
+
+	bool useMP_;
+
+	size_t iteration_;
+
+
+};
+
+}
+}
+
+#endif /* INCLUDE_CT_RBD_SLQ_FLOATINGBASESLQ_H_ */
---
+++ ct_rbd/src/kindrEulerInverseRotateTest.cpp
@@ -0,0 +1,72 @@
+/*
+ * kindrEulerInverseRotateTest.cpp
+ *
+ *  Created on: Feb 13, 2017
+ *      Author: neunertm
+ */
+
+#include <chrono>
+#include <kindr/Core>
+
+int main()
+{
+
+	static const size_t nTests = 1000000;
+	std::vector<kindr::EulerAnglesXyzD, Eigen::aligned_allocator<kindr::EulerAnglesXyzD> > angles(nTests);
+
+	typedef std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > test_vector_t;
+	test_vector_t vectors(nTests);
+	test_vector_t vectorsRotatedNormal(nTests);
+	test_vector_t vectorsRotatedMatrix(nTests);
+
+	std::cout<< "running "<<nTests<<" rotation tests"<<std::endl;
+
+	for (size_t i=0; i<nTests; i++)
+	{
+		angles[i].setRandom();
+		vectors[i].setRandom();
+	}
+
+	std::cout << "timing angle.inverseRotate(vector)" << std::endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (size_t i=0; i<nTests; i++)
+	{
+		vectorsRotatedNormal[i] = angles[i].inverseRotate(vectors[i]);
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto diff = end - start;
+	size_t msTotal = std::chrono::duration <double, std::milli> (diff).count();
+	std::cout << "Total time: " << msTotal << " ms. Average: " << msTotal/double(nTests) << " ms" << std::endl;
+
+	std::cout << "timing angle.toRotationMatrix().inverted().rotate(vector)" << std::endl;
+	start = std::chrono::high_resolution_clock::now();
+	for (size_t i=0; i<nTests; i++)
+	{
+		vectorsRotatedMatrix[i] = kindr::RotationMatrixD(angles[i]).inverted().rotate(vectors[i]);
+	}
+	end = std::chrono::high_resolution_clock::now();
+	diff = end - start;
+	msTotal = std::chrono::duration <double, std::milli> (diff).count();
+	std::cout << "Total time: " << msTotal << " ms. Average: " << msTotal/double(nTests) << " ms" << std::endl;
+
+	double error = 0.0;
+	double maxError = 0.0;
+	size_t maxErrorIndex = 0;
+	for (size_t i=0; i<nTests; i++)
+	{
+		double err = (vectorsRotatedNormal[i]-vectorsRotatedMatrix[i]).norm();
+		if (err>maxError)
+		{
+			maxError = err;
+			maxErrorIndex = i;
+		}
+		error += err;
+	}
+	std::cout << "average error: "<<error/double(nTests)<<std::endl;
+	std::cout << "max error "<<maxError<<" for pair: "<<std::endl;
+	std::cout << "euler angles: " <<  angles[maxErrorIndex].toImplementation().transpose() << std::endl;
+	std::cout << "vector to rotate: "<< vectors[maxErrorIndex].transpose() << std::endl;
+	std::cout << "vector inverse rotated (direct): "<< vectorsRotatedNormal[maxErrorIndex].transpose() << std::endl;
+	std::cout << "vector inverse rotated (rotation matrix transpose): "<< vectorsRotatedMatrix[maxErrorIndex].transpose() << std::endl;
+
+}
