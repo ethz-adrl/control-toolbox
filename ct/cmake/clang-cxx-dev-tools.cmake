58715c1c4c99d564ca10f7c479430df50c849e3f
Michael Neunert <neunertm@gmail.com>
2017-10-25 02:25:19 +0200

    improved clang-tidy/-format a bit

--- ct/cmake/clang-cxx-dev-tools.cmake
+++ ct/cmake/clang-cxx-dev-tools.cmake
@@ -29,41 +29,41 @@ filter_ct_directories(ALL_CXX_SOURCE_FILES "/QuadrotorWithLoad/generated/")   # 
           
 
 # Adding clang-format target if executable is found
-if(CLANG_FORMAT)
-    find_program(CLANG_FORMAT_BIN "clang-format-3.9")
-    message(${CLANG_FORMAT_BIN})
-    if(NOT CLANG_FORMAT_BIN)
-        message(WARNING "CLANG-FORMAT not found")
-    else(NOT CLANG_FORMAT_BIN)
-      message (WARNING "USING CLANG-FORMAT. This re-formats the source-code in a well-defined style.")
-      add_custom_target(
-        clang-format ALL
-        COMMAND clang-format-3.9
-        -i
-        -style=file
-        ${ALL_CXX_SOURCE_FILES}
-        )
-    endif()
-endif(CLANG_FORMAT)
+find_program(CLANG_FORMAT_BIN "clang-format-3.9")
+message(${CLANG_FORMAT_BIN})
+if(NOT CLANG_FORMAT_BIN)
+    add_custom_target(clang-format
+        COMMAND ${CMAKE_COMMAND} -E echo_append "clang-format executable not found"
+        VERBATIM)
+else()
+  message (WARNING "USING CLANG-FORMAT. This re-formats the source-code in a well-defined style.")
+  add_custom_target(
+    clang-format ALL
+    COMMAND clang-format-3.9
+    -i
+    -style=file
+    ${ALL_CXX_SOURCE_FILES}
+    )
+endif()
 
 
 # Adding clang-tidy target if clang-tidy executable is found
-if(CLANG_TIDY)
-    find_program(CLANG_TIDY_BIN "clang-tidy-3.9")
-        message(${CLANG_TIDY_BIN})
-    if(NOT CLANG_TIDY_BIN)
-        message(WARNING "CLANG-TIDY not found")
-    else(NOT CLANG_TIDY_BIN)
-        message (WARNING "USING CLANG-TIDY to analyze the code for formatting issues...")
-        add_custom_target(
-            clang-tidy ALL
-            COMMAND clang-tidy-3.9 # todo how to get rid of version number?
-            ${ALL_CXX_SOURCE_FILES}
-            -config='' #uses the config file .clang-tidy (located in the root directory)
-            --
-            -std=c++11
-            ${INCLUDE_DIRECTORIES}
-            COMMENT "Launching clang-tidy"
-        )
+find_program(CLANG_TIDY_BIN "clang-tidy-3.9")
+    message(${CLANG_TIDY_BIN})
+if(NOT CLANG_TIDY_BIN)
+    add_custom_target(clang-tidy
+        COMMAND ${CMAKE_COMMAND} -E echo_append "clang-tidy executable not found"
+        VERBATIM)
+else()
+    message (WARNING "USING CLANG-TIDY to analyze the code for formatting issues...")
+    add_custom_target(
+        clang-tidy ALL
+        COMMAND clang-tidy-3.9 # todo how to get rid of version number?
+        ${ALL_CXX_SOURCE_FILES}
+        -config='' #uses the config file .clang-tidy (located in the root directory)
+        --
+        -std=c++11
+        ${INCLUDE_DIRECTORIES}
+        COMMENT "Launching clang-tidy"
+    )
 endif() #CLANG_TIDY_BIN
-endif() #CLANG_TIDY
