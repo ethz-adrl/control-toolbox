0d701b2e8afa26d5220132f79c5ed44749a62341
Jan Carius <jan.carius@gmx.de>
2017-11-26 16:40:27 +0100

    introduce DynamicsLinearizerNumDiff and use it in SystemLinearizer

--- ct_core/include/ct/core/systems/continuous_time/linear/SystemLinearizer.h
+++ ct_core/include/ct/core/systems/continuous_time/linear/SystemLinearizer.h
@@ -14,6 +14,8 @@ Licensed under Apache2 license (see LICENSE file in main directory)
 
 #include <ct/core/systems/continuous_time/ControlledSystem.h>
 
+#include <ct/core/systems/linearizer/DynamicsLinearizerNumDiff.h>
+
 #include "LinearSystem.h"
 
 namespace ct {
@@ -69,6 +71,8 @@ public:
     typedef Eigen::Matrix<SCALAR, STATE_DIM, STATE_DIM> state_matrix_t;            //!< state Jacobian type (A)
     typedef Eigen::Matrix<SCALAR, STATE_DIM, CONTROL_DIM> state_control_matrix_t;  //! control Jacobian type (B)
 
+    typedef ControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR> system_t;
+
     //! default constructor
     /*!
 	 * Initializes the linearizer with a non-linear system.
@@ -76,45 +80,33 @@ public:
 	 * @param nonlinearSystem non-linear system to linearize
 	 * @param doubleSidedDerivative if true, double sided numerical differentiation is used
 	 */
-    SystemLinearizer(std::shared_ptr<ControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR>> nonlinearSystem,
+    SystemLinearizer(std::shared_ptr<system_t> nonlinearSystem,
         bool doubleSidedDerivative = true)
         : LinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>(nonlinearSystem->getType()),
           nonlinearSystem_(nonlinearSystem),
-          doubleSidedDerivative_(doubleSidedDerivative)
+          linearizer_(std::bind(&system_t::computeControlledDynamics, nonlinearSystem_.get(), std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4),
+              doubleSidedDerivative)
     {
         if (nonlinearSystem == nullptr)
             throw std::runtime_error("SystemLinearizer: Nonlinear system is nullptr!");
 
-        if (nonlinearSystem_->getType() == SECOND_ORDER)
+        if (nonlinearSystem->getType() == SECOND_ORDER)
             isSecondOrderSystem_ = true;
         else
             isSecondOrderSystem_ = false;
 
         dFdx_.setZero();
         dFdu_.setZero();
-
-        if (isSecondOrderSystem_)
-        {
-            // fill default
-            dFdx_.template topLeftCorner<STATE_DIM / 2, STATE_DIM / 2>().setZero();
-            dFdx_.template topRightCorner<STATE_DIM / 2, STATE_DIM / 2>().setIdentity();
-
-            // fill default
-            dFdu_.template topRows<STATE_DIM / 2>().setZero();
-        }
-
-        eps_ = sqrt(Eigen::NumTraits<SCALAR>::epsilon());
     }
 
     //! copy constructor
     SystemLinearizer(const SystemLinearizer& arg)
         : LinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>(arg),
           nonlinearSystem_(arg.nonlinearSystem_->clone()),
+          linearizer_(std::bind(&system_t::computeControlledDynamics, nonlinearSystem_.get(), std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4),
+              arg.linearizer_.getDoubleSidedDerivativeFlag()),
           dFdx_(arg.dFdx_),
           dFdu_(arg.dFdu_),
-          dxdt_ref_(arg.dxdt_ref_),
-          eps_(arg.eps_),
-          doubleSidedDerivative_(arg.doubleSidedDerivative_),
           isSecondOrderSystem_(arg.getType() == SECOND_ORDER)
     {
     }
@@ -145,60 +137,12 @@ public:
         const control_vector_t& u,
         const SCALAR t = SCALAR(0.0)) override
     {
-        nonlinearSystem_->computeControlledDynamics(x, t, u, dxdt_ref_);
+        dFdx_ = linearizer_.getDerivativeState(x, u, t);
 
-        for (size_t i = 0; i < STATE_DIM; ++i)
+        if (isSecondOrderSystem_)
         {
-            state_vector_t dxdt;
-
-            // inspired from http://en.wikipedia.org/wiki/Numerical_differentiation#Practical_considerations_using_floating_point_arithmetic
-            SCALAR h = eps_ * std::max(fabs(x(i)), SCALAR(1.0));
-            SCALAR x_ph = x(i) + h;
-            SCALAR dxp = x_ph - x(i);
-
-            state_vector_t x_perturbed = x;
-            x_perturbed(i) = x_ph;
-
-            // get evaluation of f(x,u)
-            nonlinearSystem_->computeControlledDynamics(x_perturbed, t, u, dxdt);
-
-            if (doubleSidedDerivative_)
-            {
-                state_vector_t dxdt_low;
-
-                SCALAR x_mh = x(i) - h;
-                SCALAR dxm = x(i) - x_mh;
-
-                x_perturbed = x;
-                x_perturbed(i) = x_mh;
-                nonlinearSystem_->computeControlledDynamics(x_perturbed, t, u, dxdt_low);
-
-                if (isSecondOrderSystem_)
-                {
-                    dFdx_.template block<STATE_DIM / 2, 1>(STATE_DIM / 2, i) =
-                        (dxdt.template segment<STATE_DIM / 2>(STATE_DIM / 2) -
-                            dxdt_low.template segment<STATE_DIM / 2>(STATE_DIM / 2)) /
-                        (dxp + dxm);
-                }
-                else
-                {
-                    dFdx_.template col(i) = (dxdt - dxdt_low) / (dxp + dxm);
-                }
-            }
-            else
-            {
-                if (isSecondOrderSystem_)
-                {
-                    dFdx_.template block<STATE_DIM / 2, 1>(STATE_DIM / 2, i) =
-                        (dxdt.template segment<STATE_DIM / 2>(STATE_DIM / 2) -
-                            dxdt_ref_.template segment<STATE_DIM / 2>(STATE_DIM / 2)) /
-                        dxp;
-                }
-                else
-                {
-                    dFdx_.template col(i) = (dxdt - dxdt_ref_) / dxp;
-                }
-            }
+            dFdx_.template topLeftCorner <STATE_DIM / 2, STATE_DIM / 2>().setZero();
+            dFdx_.template topRightCorner<STATE_DIM / 2, STATE_DIM / 2>().setIdentity();
         }
 
         return dFdx_;
@@ -223,61 +167,11 @@ public:
         const control_vector_t& u,
         const SCALAR t = SCALAR(0.0)) override
     {
-        nonlinearSystem_->computeControlledDynamics(x, t, u, dxdt_ref_);
+        dFdu_ = linearizer_.getDerivativeControl(x, u, t);
 
-        for (size_t i = 0; i < CONTROL_DIM; ++i)
+        if (isSecondOrderSystem_)
         {
-            state_vector_t dxdt;
-
-            // inspired from http://en.wikipedia.org/wiki/Numerical_differentiation#Practical_considerations_using_floating_point_arithmetic
-            SCALAR h = eps_ * std::max(fabs(u(i)), SCALAR(1.0));
-            SCALAR u_ph = u(i) + h;
-            SCALAR dup = u_ph - u(i);
-
-            control_vector_t u_perturbed = u;
-            u_perturbed(i) = u_ph;
-
-            // get evaluation of f(x,u)
-            nonlinearSystem_->computeControlledDynamics(x, t, u_perturbed, dxdt);
-
-            if (doubleSidedDerivative_)
-            {
-                state_vector_t dxdt_low;
-
-                SCALAR u_mh = u(i) - h;
-                SCALAR dum = u_ph - u(i);
-
-                u_perturbed = u;
-                u_perturbed(i) = u_mh;
-
-                nonlinearSystem_->computeControlledDynamics(x, t, u_perturbed, dxdt_low);
-
-                if (isSecondOrderSystem_)
-                {
-                    dFdu_.template block<STATE_DIM / 2, 1>(STATE_DIM / 2, i) =
-                        (dxdt.template segment<STATE_DIM / 2>(STATE_DIM / 2) -
-                            dxdt_low.template segment<STATE_DIM / 2>(STATE_DIM / 2)) /
-                        (dup + dum);
-                }
-                else
-                {
-                    dFdu_.template col(i) = (dxdt - dxdt_low) / (dup + dum);
-                }
-            }
-            else
-            {
-                if (isSecondOrderSystem_)
-                {
-                    dFdu_.template block<STATE_DIM / 2, 1>(STATE_DIM / 2, i) =
-                        (dxdt.template segment<STATE_DIM / 2>(STATE_DIM / 2) -
-                            dxdt_ref_.template segment<STATE_DIM / 2>(STATE_DIM / 2)) /
-                        dup;
-                }
-                else
-                {
-                    dFdu_.template col(i) = (dxdt - dxdt_ref_) / dup;
-                }
-            }
+          dFdu_.template topRows<STATE_DIM / 2>().setZero();
         }
 
         return dFdu_;
@@ -288,15 +182,10 @@ protected:
     std::shared_ptr<ControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR>>
         nonlinearSystem_;  //!< instance of non-linear system
 
+    DynamicsLinearizerNumDiff<STATE_DIM,CONTROL_DIM,SCALAR,SCALAR> linearizer_; //!< instance of numerical-linearizer
+
     state_matrix_t dFdx_;          //!< Jacobian wrt state
     state_control_matrix_t dFdu_;  //!< Jacobian wrt input
-
-    StateVector<STATE_DIM, SCALAR> dxdt_ref_;  //!< reference state for numerical differentiation
-
-
-    SCALAR eps_;  //!< perturbation for numerical differentiation
-
-    bool doubleSidedDerivative_;  //!< flag if double sided numerical differentiation should be used
 
     bool isSecondOrderSystem_;  //!< flag if system is a second order system
 };
