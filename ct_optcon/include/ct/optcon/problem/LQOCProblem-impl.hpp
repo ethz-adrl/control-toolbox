1e3add6941e7a2271dc745a63d5ba30b2b9a47e9
Markus Gft <markusgft@gmail.com>
2017-11-28 00:34:00 +0100

    first draft of constrained linear quadratic OCP class

--- ct_optcon/include/ct/optcon/problem/LQOCProblem-impl.hpp
+++ ct_optcon/include/ct/optcon/problem/LQOCProblem-impl.hpp
@@ -9,10 +9,18 @@ Licensed under Apache2 license (see LICENSE file in main directory)
 namespace ct {
 namespace optcon {
 
+
 template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
 LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::LQOCProblem(int N)
+    : isConstrained_(false)  // by default, we assume the problem ins unconstrained
 {
     changeNumStages(N);
+}
+
+template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
+bool LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::isConstrained() const
+{
+    return isConstrained_;
 }
 
 template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
@@ -40,11 +48,21 @@ void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::changeNumStages(int N)
 
     rv_.resize(N);
     R_.resize(N);
+
+    u_lb_.resize(N);
+    u_ub_.resize(N);
+    x_lb_.resize(N + 1);
+    x_ub_.resize(N + 1);
+
+    d_lb_.resize(N + 1);
+    d_ub_.resize(N + 1);
+    C_.resize(N + 1);
+    D_.resize(N + 1);
 }
 
 
 template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
-void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setZero()
+void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setZero(const int& nGenConstr)
 {
     A_.setConstant(core::StateMatrix<STATE_DIM, SCALAR>::Zero());
     B_.setConstant(core::StateControlMatrix<STATE_DIM, CONTROL_DIM, SCALAR>::Zero());
@@ -57,6 +75,57 @@ void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setZero()
     rv_.setConstant(core::ControlVector<CONTROL_DIM, SCALAR>::Zero());
     R_.setConstant(core::ControlMatrix<CONTROL_DIM, SCALAR>::Zero());
     q_.setConstant((SCALAR)0.0);
+
+
+    // despite this method is called setZero() the upper and lower box constraints are set to the numeric limits
+    core::ControlVector<CONTROL_DIM, SCALAR> umin, umax;
+    umin.setConstant(std::numeric_limits<SCALAR>::lowest());
+    umax.setConstant(std::numeric_limits<SCALAR>::max());
+    u_lb_.setConstant(umin);
+    u_ub_.setConstant(umax);
+
+    core::StateVector<STATE_DIM, SCALAR> xmin, xmax;
+    xmin.setConstant(std::numeric_limits<SCALAR>::lowest());
+    xmax.setConstant(std::numeric_limits<SCALAR>::max());
+    x_lb_.setConstant(xmin);
+    x_ub_.setConstant(xmax);
+
+    assert(d_lb_.size() == d_ub_.size());
+    assert(d_lb_.size() == C_.size());
+    assert(d_lb_.size() == D_.size());
+    for (size_t i = 0; i < d_lb_.size(); i++)
+    {
+        d_lb_[i].resize(nGenConstr, 1);
+        d_lb_[i].setZero();
+        d_ub_[i].resize(nGenConstr, 1);
+        d_ub_[i].setZero();
+        C_[i].resize(nGenConstr, STATE_DIM);
+        C_[i].setZero();
+        D_[i].resize(nGenConstr, CONTROL_DIM);
+        D_[i].setZero();
+    }
+
+    isConstrained_ = false;
+}
+
+
+template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
+void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setStateBoxConstraints(ct::core::StateVector<STATE_DIM, SCALAR>& x_lb,
+    ct::core::StateVector<STATE_DIM, SCALAR>& x_ub)
+{
+    x_lb_.setConstant(x_lb);
+    x_ub_.setConstant(x_ub);
+    isConstrained_ = true;
+}
+
+template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
+void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setControlBoxConstraints(
+    ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_lb,
+    ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_ub)
+{
+    u_lb_.setConstant(u_lb);
+    u_ub_.setConstant(u_ub);
+    isConstrained_ = true;
 }
 
 template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
@@ -98,6 +167,28 @@ void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setFromTimeInvariantLinearQuad
 
     x_ = core::StateVectorArray<STATE_DIM, SCALAR>(K_ + 1, x0);
     u_ = core::ControlVectorArray<CONTROL_DIM, SCALAR>(K_, u0);
+
+    isConstrained_ = false;
+}
+
+
+template <int STATE_DIM, int CONTROL_DIM, typename SCALAR>
+void LQOCProblem<STATE_DIM, CONTROL_DIM, SCALAR>::setFromTimeInvariantLinearQuadraticProblem(
+    ct::core::StateVector<STATE_DIM, SCALAR>& x0,
+    ct::core::ControlVector<CONTROL_DIM, SCALAR>& u0,
+    ct::core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>& linearSystem,
+    ct::optcon::CostFunctionQuadratic<STATE_DIM, CONTROL_DIM, SCALAR>& costFunction,
+    ct::core::StateVector<STATE_DIM, SCALAR>& stateOffset,
+    ct::core::StateVector<STATE_DIM, SCALAR>& x_lb,
+    ct::core::StateVector<STATE_DIM, SCALAR>& x_ub,
+    ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_lb,
+    ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_ub,
+    double dt)
+{
+    setFromTimeInvariantLinearQuadraticProblem(x0, u0, linearSystem, costFunction, stateOffset, dt);
+
+    setControlBoxConstraints(u_lb, u_ub);
+    setStateBoxConstraints(x_lb, x_ub);
 }
 
 }  // namespace optcon
--- ct_optcon/include/ct/optcon/problem/LQOCProblem.hpp
+++ ct_optcon/include/ct/optcon/problem/LQOCProblem.hpp
@@ -16,7 +16,7 @@ namespace optcon {
  * - reference trajectories (arrays!) for state and control
  * - LQ approximation of the cost function
  *
- * The LQ problem hence has the following form:
+ * The unconstrained LQ problem hence has the following form:
  * \f[
  * \min_{\delta \mathbf{u}_n, \delta \mathbf{x}_n}
  * \bigg \{
@@ -37,12 +37,37 @@ namespace optcon {
  *
  * The reference trajectories for state and control are here denoted as \f$ \hat \mathbf x_i, \
  *  \hat \mathbf u_i \quad \forall i = 0, 1, \ldots \f$
+ *
+ * The constrained LQ problem additionally implements the box constraints
+ * \f$ \mathbf \x_{lb} \leq \mathbf x_n \leq \mathbf \x_{ub} \ \forall i=1,2,\ldots,N \f$ and
+ * \f$ \mathbf \u_{lb} \leq \mathbf u_n \leq \mathbf \u_{ub} \ \forall i=0,1,\ldots,N-1  \f$
+ * which in differential notation read as
+ * \f[
+ * \mathbf \x_{lb} - \hat \mathbf x_n  \leq \delta \mathbf x_n \leq \mathbf \x_{ub} - \hat \mathbf x_n \ \forall i=1,2,\ldots,N
+ * \f]
+ * and
+ * \f[
+ * \mathbf \u_{lb} - \hat \mathbf u_n  \leq \delta \mathbf u_n \leq \mathbf \u_{ub} - \hat \mathbf u_n \ \forall i=0,1,\ldots,N-1
+ * \f]
+ * and the general inequality constraints
+ * \f[
+ * \mathbf \d_{lb}  \leq \mathbf C_n \delta \mathbf \x_n + \mathbf D_n \delta \mathbf \u_n \leq \mathbf \d_{ub} \ \forall i=0,1,\ldots,N
+ * \f]
  */
 template <int STATE_DIM, int CONTROL_DIM, typename SCALAR = double>
 class LQOCProblem
 {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+	using constr_vec_t = Eigen::Matrix<SCALAR, -1, 1>;
+    using constr_state_jac_t = Eigen::Matrix<SCALAR, -1, STATE_DIM>;
+    using constr_control_jac_t = Eigen::Matrix<SCALAR, -1, CONTROL_DIM>;
+
+    using constr_vec_array_t = ct::core::DiscreteArray<constr_vec_t>;
+    using constr_state_jac_array_t =  ct::core::DiscreteArray<constr_state_jac_t>;
+    using constr_control_jac_array_t = ct::core::DiscreteArray<constr_control_jac_t>;
+
 
     //! constructor
     LQOCProblem(int N = 0);
@@ -53,15 +78,64 @@ public:
     //! change the number of discrete time steps in the LOCP
     void changeNumStages(int N);
 
-    //! set all member variables to zero
-    void setZero();
+    /*!
+     * @brief set all member variables to zero
+     * @param nGenConstr by default, we resize the general constraint containers to zero
+     */
+    void setZero(const int& nGenConstr = 0);
 
+    //! set state box constraints
+    void setStateBoxConstraints(ct::core::StateVector<STATE_DIM, SCALAR>& x_lb,
+        ct::core::StateVector<STATE_DIM, SCALAR>& x_ub);
+
+    //! set control box constraints
+    void setControlBoxConstraints(ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_lb,
+        ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_ub);
+
+    /*!
+     * \brief a convenience method which constructs an unconstrained LQOC Problem from an LTI system and continuous-time quadratic cost
+     * The discretization of the cost functions happens within this function. It employs an Euler-Discretization
+     * @param x0 the initial state
+     * @param u0 the current (and desired control)
+     * @param linearSystem the discrete-time LTI system
+     * @param costFunction the continuous-time cost function
+     * @param stateOffset the offset for the affine system dynamics demanded by the LQOC Solver
+     * @param dt the sampling time, required for discretization
+     */
     void setFromTimeInvariantLinearQuadraticProblem(ct::core::StateVector<STATE_DIM, SCALAR>& x0,
         ct::core::ControlVector<CONTROL_DIM, SCALAR>& u0,
         ct::core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>& linearSystem,
         ct::optcon::CostFunctionQuadratic<STATE_DIM, CONTROL_DIM, SCALAR>& costFunction,
         ct::core::StateVector<STATE_DIM, SCALAR>& stateOffset,
         double dt);
+
+    /*!
+     * \brief a convenience method which constructs a box-constrained LQOC Problem from an LTI system and continuous-time quadratic cost
+     * The discretization of the cost functions and constraints happens within this function. It employs an Euler-Discretization
+     * @param x0 the initial state
+     * @param u0 the current (and desired control)
+     * @param linearSystem the discrete-time LTI system
+     * @param costFunction the continuous-time cost function
+     * @param stateOffset the offset for the affine system dynamics demanded by the LQOC Solver
+     * @param dt the sampling time, required for discretization
+     * @param x_lb state lower bound
+     * @param x_ub state upper bound
+     * @param u_lb control lower bound
+     * @param u_ub control upper bound
+     */
+    void setFromTimeInvariantLinearQuadraticProblem(ct::core::StateVector<STATE_DIM, SCALAR>& x0,
+        ct::core::ControlVector<CONTROL_DIM, SCALAR>& u0,
+        ct::core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>& linearSystem,
+        ct::optcon::CostFunctionQuadratic<STATE_DIM, CONTROL_DIM, SCALAR>& costFunction,
+        ct::core::StateVector<STATE_DIM, SCALAR>& stateOffset,
+        ct::core::StateVector<STATE_DIM, SCALAR>& x_lb,
+        ct::core::StateVector<STATE_DIM, SCALAR>& x_ub,
+        ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_lb,
+        ct::core::ControlVector<CONTROL_DIM, SCALAR>& u_ub,
+        double dt);
+
+    //! return a flag indicating whether this LQOC Problem is constrained or not
+    bool isConstrained() const;
 
     //! affine, time-varying system dynamics in discrete time
     ct::core::StateMatrixArray<STATE_DIM, SCALAR> A_;
@@ -88,9 +162,32 @@ public:
     //! LQ approximation of the cross terms of the cost function
     ct::core::FeedbackArray<STATE_DIM, CONTROL_DIM, SCALAR> P_;
 
+
+    //! lower bound of control input box constraint
+    ct::core::ControlVectorArray<CONTROL_DIM, SCALAR> u_lb_;
+    //! upper bound of control input box constraint
+    ct::core::ControlVectorArray<CONTROL_DIM, SCALAR> u_ub_;
+
+    //! lower bound of state box constraint
+    ct::core::StateVectorArray<STATE_DIM, SCALAR> x_lb_;
+    //! upper bound of state box constraint
+    ct::core::StateVectorArray<STATE_DIM, SCALAR> x_ub_;
+
+    //! general constraint lower bound
+    constr_vec_array_t d_lb_;
+    //! general constraint upper bound
+    constr_vec_array_t d_ub_;
+
+    //! linear general constraint matrices
+    constr_state_jac_array_t C_;
+    constr_control_jac_array_t D_;
+
 private:
     //! the number of discrete time steps in the LOCP, including terminal stage
     int K_;
+
+    //! bool indicating if the optimization problem is constrained
+    bool isConstrained_;
 };
 
 }  // namespace optcon
