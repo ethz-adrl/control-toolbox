6425d5026f5ea6e9f4ef965a1713a4abd9b0150b
Markus Gft <markusgft@gmail.com>
2017-12-05 11:57:53 +0100

    bugfix: ConstraintContainerAD can now be cloned correctly !!!

--- ct_optcon/include/ct/optcon/constraint/ConstraintContainerAD-impl.h
+++ ct_optcon/include/ct/optcon/constraint/ConstraintContainerAD-impl.h
@@ -42,11 +42,7 @@ ConstraintContainerAD<STATE_DIM, CONTROL_DIM, SCALAR>::ConstraintContainerAD(con
 
 template <size_t STATE_DIM, size_t CONTROL_DIM, typename SCALAR>
 ConstraintContainerAD<STATE_DIM, CONTROL_DIM, SCALAR>::ConstraintContainerAD(const ConstraintContainerAD& arg)
-    : LinearConstraintContainer<STATE_DIM, CONTROL_DIM>(arg),
-      constraintsIntermediate_(arg.constraintsIntermediate_),
-      constraintsTerminal_(arg.constraintsTerminal_),
-      intermediateCodegen_(arg.intermediateCodegen_),
-      terminalCodegen_(arg.terminalCodegen_),
+    : LinearConstraintContainer<STATE_DIM, CONTROL_DIM, SCALAR>(arg),
       fIntermediate_(arg.fIntermediate_),
       fTerminal_(arg.fTerminal_),
       sparsityIntermediateRows_(arg.sparsityIntermediateRows_),
@@ -71,6 +67,15 @@ ConstraintContainerAD<STATE_DIM, CONTROL_DIM, SCALAR>::ConstraintContainerAD(con
     for (size_t i = 0; i < constraintsTerminal_.size(); ++i)
         constraintsTerminal_[i] =
             std::shared_ptr<ConstraintBase<STATE_DIM, CONTROL_DIM, SCALAR>>(arg.constraintsTerminal_[i]->clone());
+
+    intermediateCodegen_ =
+        std::shared_ptr<JacCG>(new JacCG(fIntermediate_, STATE_DIM + CONTROL_DIM, getIntermediateConstraintsCount()));
+    terminalCodegen_ =
+        std::shared_ptr<JacCG>(new JacCG(fTerminal_, STATE_DIM + CONTROL_DIM, getTerminalConstraintsCount()));
+
+    // make sure libraries get compiled in clone
+    initializeIntermediate();
+    initializeTerminal();
 }
 
 
