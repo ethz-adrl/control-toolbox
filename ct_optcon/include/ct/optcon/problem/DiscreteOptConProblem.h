42dbb6ac1796a51b6eeeaac1c47874b79afd9526
Jan Carius <jan.carius@gmx.de>
2017-12-22 16:00:05 +0100

    wip

--- ct_optcon/include/ct/optcon/problem/DiscreteOptConProblem.h
+++ ct_optcon/include/ct/optcon/problem/DiscreteOptConProblem.h
@@ -10,11 +10,51 @@ namespace ct {
 namespace optcon {
 
 template <size_t STATE_DIM, size_t CONTROL_DIM, typename SCALAR = double>
-using DiscreteOptConProblem = OptConProblemBase<STATE_DIM,
-    CONTROL_DIM,
-    core::DiscreteControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
-    core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
-    SCALAR>;
+class DiscreteOptConProblem : public ct::optcon::OptConProblemBase<STATE_DIM,
+                                  CONTROL_DIM,
+                                  core::DiscreteControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
+                                  core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
+                                  SCALAR>
+{
+    typedef OptConProblemBase<STATE_DIM,
+        CONTROL_DIM,
+        core::DiscreteControlledSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
+        core::DiscreteLinearSystem<STATE_DIM, CONTROL_DIM, SCALAR>,
+        SCALAR>
+        Base;
+    using Base::Base;  // inherit all constructors
+
+    /*!
+     * @brief Construct a discrete Optimal Control Problem from a continuous one
+     *
+     * @param continuousProblem the continuous Optimal Control Problem
+     * @param dt the discretization time interval
+     */
+    DiscreteOptConProblem(const OptConProblem& continuousProblem, const double dt)
+    {
+        continuousProblem.verify();  // ensure the continuous problem is complete
+
+        // TODO: we might not need this at all
+        std::shared_ptr<SystemDiscretizer<STATE_DIM, CONTROL_DIM>> discreteSys;
+        discreteSys.reset(new SystemDiscretizer<STATE_DIM, CONTROL_DIM>(continuousProblem.getNonlinearSystem(), dt));
+
+        setNonlinearSystem(discreteSys);
+
+        //TODO need controller
+        std::shared_ptr<SensitivityIntegrator<STATE_DIM,CONTROL_DIM>> discreteLinSys;
+        discreteLinSys.reset(new SensitivityIntegrator<STATE_DIM,CONTROL_DIM>(dt, continuousProblem.getLinearSystem()))
+        setLinearSystem(discreteLinSys);
+
+        //TODO discretize the cost function
+        //setCostFunction(...);
+        throw std::runtime_error("Not yet implemented");
+
+        setBoxConstraints(continuousProblem.getBoxConstraints());
+        setGeneralConstraints(continuousProblem.getGeneralConstraints());
+        setInitialState(continuousProblem.getInitialState());
+        setTimeHorizon(static_cast<time_t>(continuousProblem.getTimeHorizon() / dt));
+    }
+};
 
 }  // namespace optcon
 }  // namespace ct
