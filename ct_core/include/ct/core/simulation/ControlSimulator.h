fbb415930f50331ad22dd1b71114a134248f80e9
Hamza Merzic <hamzamerzic@gmail.com>
2018-01-03 12:13:50 +0100

    Fix formatting.

--- ct_core/include/ct/core/simulation/ControlSimulator.h
+++ ct_core/include/ct/core/simulation/ControlSimulator.h
@@ -27,136 +27,140 @@ namespace core {
  * @tparam CONTROLLED_SYSTEM the controlled system that we wish to simulate
  */
 template <class CONTROLLED_SYSTEM>
-class ControlSimulator {
+class ControlSimulator
+{
 public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  static const size_t STATE_DIM = CONTROLLED_SYSTEM::STATE_DIM;
-  static const size_t CONTROL_DIM = CONTROLLED_SYSTEM::CONTROL_DIM;
+    static const size_t STATE_DIM   = CONTROLLED_SYSTEM::STATE_DIM;
+    static const size_t CONTROL_DIM = CONTROLLED_SYSTEM::CONTROL_DIM;
 
-  using SCALAR = typename CONTROLLED_SYSTEM::SCALAR;
+    using SCALAR = typename CONTROLLED_SYSTEM::SCALAR;
 
-  //! default constructor
-  ControlSimulator() {}
-
-  //! constructor
-  ControlSimulator(Time sim_dt, Time control_dt,
-                   const StateVector<STATE_DIM>& x0,
-                   std::shared_ptr<CONTROLLED_SYSTEM> controlled_system,
-                   std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>> controller = nullptr)
-      : sim_dt_(sim_dt),
-        control_dt_(control_dt),
-        x0_(x0),
-        system_(controlled_system),
-        controller_(controller),
-        stop_(false) {
-    if (sim_dt_ <= 0 || control_dt_ <= 0) throw "Step sizes must be positive.";
-    if (sim_dt_ > control_dt_) throw "Simulation step must be smaller than the control step.";
-    if (controller_)
-      system_->setController(controller_);
-  }
-
-  //! copy constructor
-  ControlSimulator(const ControlSimulator& arg)
-      : sim_dt_(arg.sim_dt_), control_dt_(arg.control_dt_), x0_(arg.x0_), stop_(arg.stop_) {
-    if (!arg.system_) return;
-    system_ = std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>>(arg.system_->clone());
-    if (arg.controller_) {
-      controller_ =
-          std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>>(arg.controller_->clone());
-      system_->setController(controller_);
+    //! default constructor
+    ControlSimulator() {}
+    //! constructor
+    ControlSimulator(Time sim_dt,
+        Time control_dt,
+        const StateVector<STATE_DIM>& x0,
+        std::shared_ptr<CONTROLLED_SYSTEM> controlled_system,
+        std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>> controller = nullptr)
+        : sim_dt_(sim_dt),
+          control_dt_(control_dt),
+          x0_(x0),
+          system_(controlled_system),
+          controller_(controller),
+          stop_(false)
+    {
+        if (sim_dt_ <= 0 || control_dt_ <= 0) throw "Step sizes must be positive.";
+        if (sim_dt_ > control_dt_) throw "Simulation step must be smaller than the control step.";
+        if (controller_) system_->setController(controller_);
     }
-  }
 
-  //! destructor
-  virtual ~ControlSimulator() {};
+    //! copy constructor
+    ControlSimulator(const ControlSimulator& arg)
+        : sim_dt_(arg.sim_dt_), control_dt_(arg.control_dt_), x0_(arg.x0_), stop_(arg.stop_)
+    {
+        if (!arg.system_) return;
+        system_ = std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>>(arg.system_->clone());
+        if (arg.controller_)
+        {
+            controller_ = std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>>(arg.controller_->clone());
+            system_->setController(controller_);
+        }
+    }
 
-  //! During controller update, this method does processing before the state measurement arrives
-  virtual bool prepareControllerIteration(Time sim_time) = 0;
+    //! destructor
+    virtual ~ControlSimulator(){};
 
-  //! During controller update, this method does processing once the state measurement arrives
-  virtual bool finishControllerIteration(Time sim_time) = 0;
+    //! During controller update, this method does processing before the state measurement arrives
+    virtual bool prepareControllerIteration(Time sim_time) = 0;
 
-  //! This methods spawns the two threads
-  void simulate(Time duration) {
-    stop_ = false;
-    sim_start_time_ = std::chrono::high_resolution_clock::now();
-    x_ = x0_;
-    std::thread sys_thread(&ControlSimulator::simulateSystem, this, duration),
-        solver_thread(&ControlSimulator::simulateController, this, duration);
+    //! During controller update, this method does processing once the state measurement arrives
+    virtual bool finishControllerIteration(Time sim_time) = 0;
 
-    sys_thread.join();
-    solver_thread.join();
-  }
+    //! This methods spawns the two threads
+    void simulate(Time duration)
+    {
+        stop_           = false;
+        sim_start_time_ = std::chrono::high_resolution_clock::now();
+        x_              = x0_;
+        std::thread sys_thread(&ControlSimulator::simulateSystem, this, duration),
+            solver_thread(&ControlSimulator::simulateController, this, duration);
 
-  //! stop the simulation
-  void stop() {
-    stop_ = true;
-  }
+        sys_thread.join();
+        solver_thread.join();
+    }
 
+    //! stop the simulation
+    void stop() { stop_ = true; }
 protected:
+    //! method run by the thread that simulates the system
+    virtual void simulateSystem(Time duration)
+    {
+        Integrator<STATE_DIM> integrator(system_);
+        Time sim_time;
+        auto wall_time                = sim_start_time_;
+        StateVector<STATE_DIM> temp_x = x_;
+        // In case an integer number of steps cannot be performed
+        const double residue = control_dt_ / sim_dt_ - int(control_dt_ / sim_dt_);
 
-  //! method run by the thread that simulates the system
-  virtual void simulateSystem(Time duration) {
-    Integrator<STATE_DIM> integrator(system_);
-    Time sim_time;
-    auto wall_time = sim_start_time_;
-    StateVector<STATE_DIM> temp_x = x_;
-    // In case an integer number of steps cannot be performed
-    const double residue = control_dt_ / sim_dt_ - int(control_dt_ / sim_dt_);
+        while (std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count() <
+                   duration &&
+               !stop_)
+        {
+            sim_time = std::chrono::duration<double>(wall_time - sim_start_time_).count();
 
-    while (
-        std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_)
-            .count() < duration && !stop_) {
-      sim_time = std::chrono::duration<double>(wall_time - sim_start_time_).count();
+            integrator.integrate_n_steps(temp_x, sim_time, int(control_dt_ / sim_dt_), sim_dt_);
+            if (residue > 1e-6)
+                integrator.integrate_n_steps(temp_x, sim_time + int(control_dt_ / sim_dt_) * sim_dt_, 1, residue);
 
-      integrator.integrate_n_steps(temp_x, sim_time, int(control_dt_ / sim_dt_), sim_dt_);
-      if (residue > 1e-6)
-        integrator.integrate_n_steps(temp_x, sim_time + int(control_dt_ / sim_dt_) * sim_dt_, 1, residue);
+            // TODO: Add check if it fails to work in real time.
+            while (std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - wall_time).count() <
+                   control_dt_)
+                usleep(100);
+            wall_time += std::chrono::duration_cast<std::chrono::system_clock::duration>(
+                std::chrono::duration<double>(control_dt_));
 
-      // TODO: Add check if it fails to work in real time.
-      while (std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - wall_time)
-                 .count() < control_dt_)
-        usleep(100);
-      wall_time += std::chrono::duration_cast<std::chrono::system_clock::duration>(
-          std::chrono::duration<double>(control_dt_));
+            state_mtx_.lock();
+            x_ = temp_x;
+            state_mtx_.unlock();
 
-      state_mtx_.lock();
-      x_ = temp_x;
-      state_mtx_.unlock();
-
-      policy_mtx_.lock();
-      system_->setController(controller_);
-      policy_mtx_.unlock();
+            policy_mtx_.lock();
+            system_->setController(controller_);
+            policy_mtx_.unlock();
+        }
     }
-  }
 
-  //! method run by the thread that updates the controller
-  virtual void simulateController(Time duration) {
-    Time sim_time;
-    while (
-        std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_)
-            .count() < duration && !stop_) {
-      sim_time = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count();
-      bool success = prepareControllerIteration(sim_time);
-      sim_time = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count();
-      success &= finishControllerIteration(sim_time);
+    //! method run by the thread that updates the controller
+    virtual void simulateController(Time duration)
+    {
+        Time sim_time;
+        while (std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count() <
+                   duration &&
+               !stop_)
+        {
+            sim_time =
+                std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count();
+            bool success = prepareControllerIteration(sim_time);
+            sim_time =
+                std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - sim_start_time_).count();
+            success &= finishControllerIteration(sim_time);
 
-      if (!success)
-        throw "Simulation failed!";
+            if (!success) throw "Simulation failed!";
+        }
     }
-  }
 
-  Time sim_dt_;
-  Time control_dt_;
-  std::shared_ptr<CONTROLLED_SYSTEM> system_;
-  std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>> controller_;
-  std::chrono::time_point<std::chrono::high_resolution_clock> sim_start_time_;
-  StateVector<STATE_DIM> x0_;
-  StateVector<STATE_DIM> x_;
-  std::mutex state_mtx_;
-  std::mutex policy_mtx_;
-  std::atomic<bool> stop_;
+    Time sim_dt_;
+    Time control_dt_;
+    std::shared_ptr<CONTROLLED_SYSTEM> system_;
+    std::shared_ptr<Controller<STATE_DIM, CONTROL_DIM, SCALAR>> controller_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> sim_start_time_;
+    StateVector<STATE_DIM> x0_;
+    StateVector<STATE_DIM> x_;
+    std::mutex state_mtx_;
+    std::mutex policy_mtx_;
+    std::atomic<bool> stop_;
 };
 
 }  // namespace core
--- ct_models/examples/InvertedPendulum/NLOC_MPC.cpp
+++ ct_models/examples/InvertedPendulum/NLOC_MPC.cpp
@@ -12,7 +12,7 @@ Licensed under Apache2 license (see LICENSE file in main directory)
 
 using namespace ct::rbd;
 
-const size_t njoints = ct::rbd::InvertedPendulum::Kinematics::NJOINTS;
+const size_t njoints            = ct::rbd::InvertedPendulum::Kinematics::NJOINTS;
 const size_t actuator_state_dim = 1;
 
 typedef ct::rbd::InvertedPendulum::tpl::Dynamics<double> IPDynamics;
@@ -22,34 +22,40 @@ typedef ct::core::LinearSystem<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM, doubl
 
 using InvertedPendulumNLOC = FixBaseNLOC<IPSystem, actuator_state_dim, double>;
 
-class MPCSimulator : public ct::core::ControlSimulator<IPSystem> {
- public:
-  MPCSimulator(ct::core::Time sim_dt, ct::core::Time control_dt,
-               const ct::core::StateVector<STATE_DIM>& x0, std::shared_ptr<IPSystem> ip_system,
-               ct::optcon::MPC<ct::optcon::NLOptConSolver<STATE_DIM, CONTROL_DIM>>& mpc)
-      : ct::core::ControlSimulator<IPSystem>(sim_dt, control_dt, x0, ip_system), mpc_(mpc) {
-    controller_ = std::make_shared<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>>();
-  }
+class MPCSimulator : public ct::core::ControlSimulator<IPSystem>
+{
+public:
+    MPCSimulator(ct::core::Time sim_dt,
+        ct::core::Time control_dt,
+        const ct::core::StateVector<STATE_DIM>& x0,
+        std::shared_ptr<IPSystem> ip_system,
+        ct::optcon::MPC<ct::optcon::NLOptConSolver<STATE_DIM, CONTROL_DIM>>& mpc)
+        : ct::core::ControlSimulator<IPSystem>(sim_dt, control_dt, x0, ip_system), mpc_(mpc)
+    {
+        controller_ = std::make_shared<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>>();
+    }
 
-  virtual bool prepareControllerIteration(ct::core::Time sim_time) {
-    mpc_.prepareIteration(sim_time);
-    return true;
-  }
+    virtual bool prepareControllerIteration(ct::core::Time sim_time)
+    {
+        mpc_.prepareIteration(sim_time);
+        return true;
+    }
 
-  virtual bool finishControllerIteration(ct::core::Time sim_time) {
-    state_mtx_.lock();
-    ct::core::StateVector<STATE_DIM> x_temp = x_;
-    state_mtx_.unlock();
+    virtual bool finishControllerIteration(ct::core::Time sim_time)
+    {
+        state_mtx_.lock();
+        ct::core::StateVector<STATE_DIM> x_temp = x_;
+        state_mtx_.unlock();
 
-    std::shared_ptr<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>> new_controller =
-        std::make_shared<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>>();
-    bool success = mpc_.finishIteration(x_temp, sim_time, *new_controller, controller_ts_);
+        std::shared_ptr<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>> new_controller =
+            std::make_shared<ct::core::StateFeedbackController<STATE_DIM, CONTROL_DIM>>();
+        bool success = mpc_.finishIteration(x_temp, sim_time, *new_controller, controller_ts_);
 
-    policy_mtx_.lock();
-    controller_ = new_controller;
-    policy_mtx_.unlock();
-    return success;
-  }
+        policy_mtx_.lock();
+        controller_ = new_controller;
+        policy_mtx_.unlock();
+        return success;
+    }
 
 private:
     ct::optcon::MPC<ct::optcon::NLOptConSolver<STATE_DIM, CONTROL_DIM>>& mpc_;
@@ -63,7 +69,7 @@ int main(int argc, char* argv[])
     {
         std::string workingDirectory = ct::models::exampleDir + "/InvertedPendulum";
 
-        std::string configFile = workingDirectory + "/solver.info";
+        std::string configFile       = workingDirectory + "/solver.info";
         std::string costFunctionFile = workingDirectory + "/cost.info";
 
         std::shared_ptr<ct::rbd::SEADynamicsFirstOrder<njoints, double>> actuatorDynamics(
@@ -86,7 +92,7 @@ int main(int argc, char* argv[])
 
         std::shared_ptr<ct::optcon::CostFunctionAnalytical<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>> newCost =
             std::make_shared<ct::optcon::CostFunctionAnalytical<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>>();
-        size_t intTermID = newCost->addIntermediateTerm(termQuadInterm);
+        size_t intTermID   = newCost->addIntermediateTerm(termQuadInterm);
         size_t finalTermID = newCost->addFinalTerm(termQuadFinal);
 
         ct::core::Time timeHorizon;
@@ -101,16 +107,15 @@ int main(int argc, char* argv[])
         ct::core::StateVector<IPSystem::STATE_DIM> x0full = IPSystem::toFullState(x0.toImplementation());
 
         std::shared_ptr<LinearSystem> linSystem = nullptr;
-        ct::optcon::OptConProblem<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM> optConProblem(timeHorizon, x0full, ipSystem, newCost, linSystem);
+        ct::optcon::OptConProblem<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM> optConProblem(
+            timeHorizon, x0full, ipSystem, newCost, linSystem);
         InvertedPendulumNLOC nloc_solver(newCost, nloc_settings, ipSystem, verbose, linSystem);
 
         int K = nloc_solver.getSettings().computeK(timeHorizon);
 
-
         InvertedPendulumNLOC::StateVectorArray stateRefTraj(K + 1, x0full);
         InvertedPendulumNLOC::FeedbackArray fbTrajectory(K, -fbD);
-        InvertedPendulumNLOC::ControlVectorArray ffTrajectory(K,
-            InvertedPendulumNLOC::ControlVector::Zero());
+        InvertedPendulumNLOC::ControlVectorArray ffTrajectory(K, InvertedPendulumNLOC::ControlVector::Zero());
 
         int initType = 0;
         ct::core::loadScalar(configFile, "initType", initType);
@@ -122,9 +127,9 @@ int main(int argc, char* argv[])
                 ct::core::ControlVector<IPSystem::CONTROL_DIM> uff_ref;
                 nloc_solver.initializeSteadyPose(x0, timeHorizon, K, uff_ref, -fbD);
 
-                std::vector<std::shared_ptr<
-                    ct::optcon::CostFunctionQuadratic<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>>>& inst1 =
-                    nloc_solver.getSolver()->getCostFunctionInstances();
+                std::vector<
+                    std::shared_ptr<ct::optcon::CostFunctionQuadratic<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>>>&
+                    inst1 = nloc_solver.getSolver()->getCostFunctionInstances();
 
                 for (size_t i = 0; i < inst1.size(); i++)
                 {
@@ -147,28 +152,28 @@ int main(int argc, char* argv[])
         std::cout << "waiting 1 second for begin" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-
         nloc_solver.solve();
-        ct::core::StateFeedbackController<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM> initialSolution = nloc_solver.getSolution();
+        ct::core::StateFeedbackController<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM> initialSolution =
+            nloc_solver.getSolution();
         InvertedPendulumNLOC::StateVectorArray x_nloc = initialSolution.x_ref();
 
         // nloc_solver.retrieveLastRollout();
 
         ct::optcon::NLOptConSettings ilqr_settings_mpc(nloc_solver.getSettings());
         ilqr_settings_mpc.max_iterations = 1;
-        ilqr_settings_mpc.printSummary = false;
+        ilqr_settings_mpc.printSummary   = false;
 
         ct::optcon::mpc_settings mpc_settings;
-        mpc_settings.stateForwardIntegration_ = false;
-        mpc_settings.postTruncation_ = false;
-        mpc_settings.measureDelay_ = false;
+        mpc_settings.stateForwardIntegration_    = false;
+        mpc_settings.postTruncation_             = false;
+        mpc_settings.measureDelay_               = false;
         mpc_settings.delayMeasurementMultiplier_ = 1.0;
-        mpc_settings.mpc_mode = ct::optcon::MPC_MODE::CONSTANT_RECEDING_HORIZON;
-        mpc_settings.coldStart_ = false;
-        mpc_settings.minimumTimeHorizonMpc_ = 3.0;
+        mpc_settings.mpc_mode                    = ct::optcon::MPC_MODE::CONSTANT_RECEDING_HORIZON;
+        mpc_settings.coldStart_                  = false;
+        mpc_settings.minimumTimeHorizonMpc_      = 3.0;
 
-        ct::optcon::MPC<ct::optcon::NLOptConSolver<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>>
-            ilqr_mpc(optConProblem, ilqr_settings_mpc, mpc_settings);
+        ct::optcon::MPC<ct::optcon::NLOptConSolver<IPSystem::STATE_DIM, IPSystem::CONTROL_DIM>> ilqr_mpc(
+            optConProblem, ilqr_settings_mpc, mpc_settings);
         ilqr_mpc.setInitialGuess(initialSolution);
 
         MPCSimulator mpc_sim(1e-3, 1e-2, x0full, ipSystem, ilqr_mpc);
@@ -177,8 +182,7 @@ int main(int argc, char* argv[])
 
         ilqr_mpc.printMpcSummary();
 
-    }
-    catch (std::runtime_error& e)
+    } catch (std::runtime_error& e)
     {
         std::cout << "Exception caught: " << e.what() << std::endl;
     }
