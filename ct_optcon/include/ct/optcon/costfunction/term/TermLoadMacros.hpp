09612bed699929cab46ec6ff593890a0be05e78a
Markus Gft <markusgft@gmail.com>
2018-01-16 20:59:46 +0100

    make term mixed loadable from file

--- ct_optcon/include/ct/optcon/costfunction/term/TermLoadMacros.hpp
+++ ct_optcon/include/ct/optcon/costfunction/term/TermLoadMacros.hpp
@@ -4,6 +4,7 @@
 #include "TermLinear.hpp"
 #include "TermQuadratic.hpp"
 #include "TermQuadMult.hpp"
+#include "TermMixed.hpp"
 
 #define CT_LOADABLE_TERM(SCALAR_EVAL, SCALAR, TERM, TERMNAME)                      \
     if (termKind == TERMNAME)                                                      \
@@ -16,4 +17,5 @@
 #define CT_LOADABLE_TERMS(SCALAR_EVAL, SCALAR)                        \
     CT_LOADABLE_TERM(SCALAR_EVAL, SCALAR, TermLinear, "linear")       \
     CT_LOADABLE_TERM(SCALAR_EVAL, SCALAR, TermQuadratic, "quadratic") \
+    CT_LOADABLE_TERM(SCALAR_EVAL, SCALAR, TermMixed, "mixed") \
     CT_LOADABLE_TERM(SCALAR_EVAL, SCALAR, TermQuadMult, "quadratic-multiplicative")
