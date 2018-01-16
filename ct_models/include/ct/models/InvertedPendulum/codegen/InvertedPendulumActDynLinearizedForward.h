ae5e08b62db2fd4d94e1d3d26ebd50a50ffbc39c
Markus Gft <markusgft@gmail.com>
2018-01-16 18:09:05 +0100

    generating derivative code for inverted pendulum with actuator dynamics in the loop

---
+++ ct_models/include/ct/models/InvertedPendulum/codegen/InvertedPendulumActDynLinearizedForward.h
@@ -0,0 +1,59 @@
+/**********************************************************************************************************************
+This file is part of the Control Toolbox (https://adrlab.bitbucket.io/ct), copyright by ETH Zurich, Google Inc.
+Authors:  Michael Neunert, Markus Giftthaler, Markus Stäuble, Diego Pardo, Farbod Farshidian
+Licensed under Apache2 license (see LICENSE file in main directory)
+**********************************************************************************************************************/
+
+#pragma once
+
+#include <ct/core/core.h>
+
+namespace ct {
+namespace models {
+namespace InvertedPendulum {
+
+class InvertedPendulumActDynLinearizedForward : public ct::core::LinearSystem<3, 1, double>
+{
+public:
+    typedef ct::core::LinearSystem<3, 1, double> Base;
+
+    typedef typename Base::state_vector_t state_vector_t;
+    typedef typename Base::control_vector_t control_vector_t;
+    typedef typename Base::state_matrix_t state_matrix_t;
+    typedef typename Base::state_control_matrix_t state_control_matrix_t;
+
+    InvertedPendulumActDynLinearizedForward(const ct::core::SYSTEM_TYPE& type = ct::core::SYSTEM_TYPE::GENERAL)
+        : ct::core::LinearSystem<3, 1>(type)
+    {
+        initialize();
+    }
+
+    InvertedPendulumActDynLinearizedForward(const InvertedPendulumActDynLinearizedForward& other) { initialize(); }
+    virtual ~InvertedPendulumActDynLinearizedForward(){};
+
+    virtual InvertedPendulumActDynLinearizedForward* clone() const override { return new InvertedPendulumActDynLinearizedForward; }
+    virtual const state_matrix_t& getDerivativeState(const state_vector_t& x,
+        const control_vector_t& u,
+        const double t = double(0.0)) override;
+
+    virtual const state_control_matrix_t& getDerivativeControl(const state_vector_t& x,
+        const control_vector_t& u,
+        const double t = double(0.0)) override;
+
+private:
+    void initialize()
+    {
+        dFdx_.setZero();
+        dFdu_.setZero();
+        vX_.fill(0.0);
+        vU_.fill(0.0);
+    }
+
+    state_matrix_t dFdx_;
+    state_control_matrix_t dFdu_;
+    std::array<double, 0> vX_;
+    std::array<double, 0> vU_;
+};
+}
+}
+}
