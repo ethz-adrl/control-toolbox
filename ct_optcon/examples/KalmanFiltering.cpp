318b73f6eae8cdc97437497edd00b327c043c789
Hamza Merzic <hamzamerzic@gmail.com>
2018-03-25 12:38:31 +0200

    Include controller from ct_core examples.

--- ct_optcon/examples/KalmanFiltering.cpp
+++ ct_optcon/examples/KalmanFiltering.cpp
@@ -9,51 +9,8 @@
 #include <iomanip>
 #include <random>
 #include <ct/optcon/optcon.h>
-
-/*!
- * A simple custom PD controller for an oscillator
- *
- */
-//! an example controller class that takes a 2-dimensional state and outputs a 1-dimensional control action
-class CustomController : public ct::core::Controller<2, 1>
-{
-public:
-    static const size_t STATE_DIM   = 2;  // two states
-    static const size_t CONTROL_DIM = 1;  // one control action
-
-    //! default constructor
-    CustomController(const ct::core::ControlVector<CONTROL_DIM>& uff,  // feedforward control
-        const double& kp,                                              // P gain
-        const double& kd                                               // D gain
-        )
-        : uff_(uff), kp_(kp), kd_(kd)
-    {
-    }
-
-    //! destructor
-    ~CustomController() {}
-    //! copy constructor
-    CustomController(const CustomController& other) : uff_(other.uff_), kp_(other.kp_), kd_(other.kd_) {}
-    //! clone method, needs to be implemented, overrides ct::core::Controller::clone()
-    CustomController* clone() const override
-    {
-        return new CustomController(*this);  // calls copy constructor
-    }
-
-    //! override the compute control method with a custom control law
-    void computeControl(const ct::core::StateVector<STATE_DIM>& state,
-        const double& t,
-        ct::core::ControlVector<CONTROL_DIM>& controlAction) override
-    {
-        controlAction = uff_;                                 // apply feedforward control
-        controlAction(0) -= kp_ * state(0) + kd_ * state(1);  // add feedback control
-    }
-
-private:
-    ct::core::ControlVector<CONTROL_DIM> uff_;  //! feedforward control action
-    double kp_;                                 //! P gain
-    double kd_;                                 //! D gain
-};
+// Using the custom controller from ct_core examples.
+#include <CustomController.h>
 
 int main(int argc, char** argv)
 {
