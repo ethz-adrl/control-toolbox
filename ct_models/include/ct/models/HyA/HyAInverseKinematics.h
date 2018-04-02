8ec885dcd410718319d2fb4e10c86c8fcc8b1b4d
Markus Gft <markusgft@gmail.com>
2018-04-02 21:30:08 +0200

    changing return type of IK-interface in order to avoid error-throwing. Added more documentation to IK interface. adapted HyA and IRB IK classes and pushed down interface changes to unit tests.

--- ct_models/include/ct/models/HyA/HyAInverseKinematics.h
+++ ct_models/include/ct/models/HyA/HyAInverseKinematics.h
@@ -24,43 +24,58 @@ template <typename SCALAR = double>
 class HyAInverseKinematics : public InverseKinematicsBase<6, SCALAR>
 {
 public:
-    virtual std::vector<typename JointState<6, SCALAR>::Position> computeInverseKinematics(
-        const tpl::RigidBodyPose<SCALAR>& eeBasePose,
-        const std::vector<SCALAR>& freeJoints = std::vector<SCALAR>()) const
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    using BASE = InverseKinematicsBase<6, SCALAR>;
+    using JointPosition_t = typename BASE::JointPosition_t;
+    using JointPositionsVector_t = typename BASE::JointPositionsVector_t;
+    using RigidBodyPoseTpl = typename BASE::RigidBodyPoseTpl;
+
+    HyAInverseKinematics() = default;
+
+    virtual ~HyAInverseKinematics() {}
+    virtual bool computeInverseKinematics(JointPositionsVector_t& res,
+        const RigidBodyPoseTpl& eeBasePose,
+        const std::vector<size_t>& freeJoints = std::vector<size_t>()) const
     {
+        res.clear();
         IkSolutionList<double> solutions;
 
         if (size_t(hya_ik::GetNumFreeParameters()) != freeJoints.size())
-            throw std::runtime_error("Error specifying free joints");
+            throw std::runtime_error("Error specifying free joints in HyAInverseKinematics");
 
         // Data needs to be in row-major form.
         Eigen::Matrix<SCALAR, 3, 3, Eigen::RowMajor> eeBaseRotationRowMajor =
             eeBasePose.getRotationMatrix().toImplementation();
+        const std::vector<double> freeJoints_ikf(freeJoints.begin(), freeJoints.end());
         hya_ik::ComputeIk(eeBasePose.position().toImplementation().data(), eeBaseRotationRowMajor.data(),
-            freeJoints.size() > 0 ? freeJoints.data() : nullptr, solutions);
+            freeJoints_ikf.size() > 0 ? freeJoints_ikf.data() : nullptr, solutions);
 
         size_t num_solutions = solutions.GetNumSolutions();
-        std::vector<typename JointState<6, SCALAR>::Position> res;
 
-        JointState<6, SCALAR> sol;
+        if (num_solutions == 0)
+            return false;  // no solution found
+
+        JointState<6> sol;
         for (size_t i = 0u; i < num_solutions; ++i)
         {
             const IkSolutionBase<double>& solution = solutions.GetSolution(i);
-            solution.GetSolution(sol.getPositions().data(), freeJoints.size() > 0 ? freeJoints.data() : nullptr);
+            solution.GetSolution(
+                sol.getPositions().data(), freeJoints_ikf.size() > 0 ? freeJoints_ikf.data() : nullptr);
             sol.toUniquePosition(ct::models::HyA::jointLowerLimit());
             if (sol.checkPositionLimits(ct::models::HyA::jointLowerLimit(), ct::models::HyA::jointUpperLimit()))
                 res.push_back(sol.getPositions());
         }
 
-        return res;
+        return true;
     }
 
-    virtual std::vector<typename JointState<6, SCALAR>::Position> computeInverseKinematics(
-        const tpl::RigidBodyPose<SCALAR>& eeWorldPose,
-        const tpl::RigidBodyPose<SCALAR>& baseWorldPose,
-        const std::vector<SCALAR>& freeJoints = std::vector<SCALAR>()) const
+    virtual bool computeInverseKinematics(JointPositionsVector_t& res,
+        const RigidBodyPoseTpl& eeWorldPose,
+        const RigidBodyPoseTpl& baseWorldPose,
+        const std::vector<size_t>& freeJoints = std::vector<size_t>()) const
     {
-        return computeInverseKinematics(eeWorldPose.inReferenceFrame(baseWorldPose), freeJoints);
+        return computeInverseKinematics(res, eeWorldPose.inReferenceFrame(baseWorldPose), freeJoints);
     }
 };
 } /* namespace rbd */
