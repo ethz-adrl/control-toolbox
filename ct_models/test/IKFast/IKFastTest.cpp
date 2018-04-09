df392afee15f24a232348c4b6486ed1813cc5705
Markus Gft <markusgft@gmail.com>
2018-04-09 10:48:33 +0200

    bugfix in HyA and irb inverse kinematics routines.

--- ct_models/test/IKFast/IKFastTest.cpp
+++ ct_models/test/IKFast/IKFastTest.cpp
@@ -43,11 +43,19 @@ TEST(HyAIKTest, IKFastTest)
 
         auto ee_pose = kin.getEEPoseInBase(0, pos);
 
-        typename ct::rbd::HyAInverseKinematics<double>::JointPositionsVector_t solutions;
-        bool solutionFound = hya_ik_solver.computeInverseKinematics(solutions, ee_pose);
-        if (solutionFound)
+        ct::rbd::RigidBodyPose basePose;
+        basePose.setIdentity();
+
+        typename ct::rbd::HyAInverseKinematics<double>::JointPositionsVector_t solutions_base;
+        typename ct::rbd::HyAInverseKinematics<double>::JointPositionsVector_t solutions_world;
+
+        bool solutionFound_base = hya_ik_solver.computeInverseKinematics(solutions_base, ee_pose);
+        bool solutionFound_world = hya_ik_solver.computeInverseKinematics(solutions_world, ee_pose, basePose);
+        ASSERT_TRUE(solutionFound_base == solutionFound_world);
+
+        if (solutionFound_base)
         {
-            for (const auto& joints : solutions)
+            for (const auto& joints : solutions_base)
             {
                 auto query_ee_pose = kin.getEEPoseInBase(0, joints);
                 ASSERT_LT(
@@ -57,6 +65,37 @@ TEST(HyAIKTest, IKFastTest)
                               .norm(),
                     1e-6);
             }
+        }
+    }
+}
+
+// Test close-to functionality of HyA inverse kinematics
+TEST(HyAIKTest, IKFastTest_closeTo)
+{
+    ct::rbd::HyAInverseKinematics<double> hya_ik_solver;
+    ct::rbd::HyA::Kinematics kin;
+    typename ct::rbd::JointState<ct::rbd::HyA::Kinematics::NJOINTS, double>::Position pos;
+    for (int i = 0; i < 100; ++i)
+    {
+        pos.setRandom();
+
+        auto ee_pose = kin.getEEPoseInBase(0, pos);
+
+        ct::rbd::RigidBodyPose basePose;
+        basePose.setIdentity();
+
+        typename ct::rbd::HyAInverseKinematics<double>::JointPosition_t ref_position;
+        ref_position.setRandom();
+        typename ct::rbd::HyAInverseKinematics<double>::JointPosition_t solution_base;
+        typename ct::rbd::HyAInverseKinematics<double>::JointPosition_t solution_world;
+
+        bool solutionFound_base = hya_ik_solver.computeInverseKinematicsCloseTo(solution_base, ee_pose, ref_position);
+        bool solutionFound_world = hya_ik_solver.computeInverseKinematicsCloseTo(solution_world, ee_pose, basePose, ref_position);
+        ASSERT_TRUE(solutionFound_base == solutionFound_world);
+
+        if (solutionFound_base)
+        {
+        	ASSERT_LT((solution_base-solution_world).array().abs().maxCoeff(), 1e-6);
         }
     }
 }
