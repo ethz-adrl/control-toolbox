4b1679f22e2a99b82efe4948dd04579a2c7fde83
acxz <17132214+acxz@users.noreply.github.com>
2020-07-11 08:30:43 -0400

    upgrade cppad/cppadcg versions

--- ct/install_cppadcg.sh
+++ ct/install_cppadcg.sh
@@ -9,9 +9,9 @@ yes Y | sudo apt-get install llvm
 ## install CppAD
 mkdir /tmp/cppadcg_deps
 cd /tmp/cppadcg_deps
-wget https://github.com/coin-or/CppAD/archive/20200000.2.tar.gz
-tar -xzf 20200000.2.tar.gz
-cd CppAD-20200000.2
+wget https://github.com/coin-or/CppAD/archive/20200000.3.tar.gz
+tar -xzf 20200000.3.tar.gz
+cd CppAD-20200000.3
 mkdir build
 cd build
 cmake -Dcppad_prefix:PATH='/usr/local' ..
@@ -21,9 +21,9 @@ sudo make install
 ## install CppADCodeGen
 git clone https://github.com/joaoleal/CppADCodeGen.git /tmp/CppADCodeGen
 cd /tmp/CppADCodeGen
-git checkout v2.4.2 ## commit matching Cppad 2020 version
+git checkout v2.4.3
 mkdir -p build
 cd build
-cmake .. #-DLLVM_VERSION=6.0
+cmake ..
 make
 sudo make install
