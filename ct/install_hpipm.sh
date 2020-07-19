57ac93914ec838fa05e015a6fb22f8c7e75853b4
Markus Giftthaler <markusgft@gmail.com>
2020-07-19 12:44:09 +0200

    removing findblasfeo.cmake and findhpipm.cmake

--- ct/install_hpipm.sh
+++ ct/install_hpipm.sh
@@ -5,7 +5,7 @@ echo "Now installing blasfeo, using a specific commit/tag (!)"
 cd /tmp
 git clone https://github.com/giaf/blasfeo.git
 cd /tmp/blasfeo
-git checkout 50dc49e # we currently are on this release
+git checkout cc90e14 # we currently are on this commit.
 make static_library
 sudo make install_static
 
@@ -14,7 +14,7 @@ echo "Now installing hpipm, using a specific commit/tag (!)"
 cd /tmp
 git clone https://github.com/giaf/hpipm.git
 cd /tmp/hpipm
-git checkout 0.1.2
+git checkout 5d9909f # we currently are on this commit.
 make static_library
 make examples
 sudo make install_static
